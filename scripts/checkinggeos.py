8a070273946b100e38c1663bed03dab8c9c3f8a9
Julián Pérez <jcpmmx@gmail.com>
2014-05-12 12:20:09 -0500

    Testing GEOS.

---
+++ scripts/checkinggeos.py
@@ -0,0 +1,8 @@
+from django.contrib.gis.geos.libgeos import lib_path, geos_version_info
+
+
+print u'----------------------------------------------------------------------'
+print u' Checking GEOS'
+print u' - lib_path: %s' % lib_path
+print u' - geos_version_info: %s' % geos_version_info()
+print u'----------------------------------------------------------------------'
